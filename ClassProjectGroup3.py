f0bddc7161482cfb0f724876db02545a88f498c3
Aiesha Esa <aesa@odin.cs.csubak.edu>
2022-11-05 14:09:06 -0700

    adding main file

---
+++ ClassProjectGroup3.py
@@ -0,0 +1,15 @@
+#####################################################################
+# CRSE: CMPS3500
+# ASGT: Class Project
+# Python Implementation: Basic Data Analysis
+# DATE: 11/05/2022
+# Student 1: Aiesha Esa
+# Student 2: Mostafa Abadi
+# Student 3: Spencer Denney
+# Student 4: Hector Martinez
+# DESCRIPTION: Implementation Basic Data Analysis Routines
+#####################################################################
+
+
+
+
