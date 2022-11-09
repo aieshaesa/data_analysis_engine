0303a4af9643844848aba27d63d3ff1c99864505
hectorm22 <hmartinez22@csub.edu>
2022-11-09 15:10:35 -0800

    File Change
    Delete group3tool.py and move its content into ClassProjectGroup3.py

--- ClassProjectGroup3.py
+++ ClassProjectGroup3.py
@@ -10,6 +10,24 @@
 # DESCRIPTION: Implementation Basic Data Analysis Routines
 #####################################################################
 
+def mean(number_list: list) -> float:
+    """Computes the mean of a list of numbers.
 
+    Args: 
+        number_list: A list containing numbers or floats.
+    
+    Returns: 
+        A float representing the mean.
+    """
 
+    sum = 0
+    length = 0
+    calculatedMean = 0
 
+    for number in number_list:
+        sum = sum + number
+        length = length + 1
+    
+    calculatedMean = sum / length
+
+    return calculatedMean
